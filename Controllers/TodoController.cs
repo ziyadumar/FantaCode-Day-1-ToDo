816ce73f4040215ddecc01f1290162403ee2256b
wafamuhammedali123 <33367269+wafamuhammedali123@users.noreply.github.com>
2017-12-23 06:33:14 -0800

    A

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -4,10 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace FantaCode.Todo.Controllers
+namespace fcdone
 {
     [Route("v1/[controller]")]
     public class TodoController : Controller
     {
+        private readonly TodoRepository todoRepository;
+        public TodoController()
+        {
+            todoRepository = new TodoRepository();
+        }
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Todo> Get() => TodoRepository.GetAll();
+
+
+        [HttpPut("{id}")]
+    }
+
+    public class Todo
+    {
+    }
+
+    internal class TodoRepository
+    {
+        internal static IEnumerable<Todo> GetAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
