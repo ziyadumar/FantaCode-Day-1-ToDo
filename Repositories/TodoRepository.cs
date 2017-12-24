503a7f0d291b7c7e02c5ded2dc9fa971ead94b72
wafamuhammedali123 <33367269+wafamuhammedali123@users.noreply.github.com>
2017-12-23 18:11:27 -0800

    Added Readme

--- Repositories/TodoRepository.cs
+++ Repositories/TodoRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using Dapper;
 
 namespace FantaCode.Todoapi.Repositories
@@ -8,62 +9,71 @@ namespace FantaCode.Todoapi.Repositories
     public class TodoRepository
     {
 
-    private string connectionString;
-    public TodoRepository()
-    {
-        connectionString = @"Server=localhost;Database=FcdOne;Trusted_Connection=true;";
-    }
-
-    public IDbConnection GetConnection()
-    {
-        return new SqlConnection(connectionString);
-    }
-
-
-    public IEnumerable<Todo> GetAll()
-    {
-        using (IDbConnection dbConnection = GetConnection())
+        private string connectionString;
+        public TodoRepository()
         {
-            dbConnection.Open();
-            return dbConnection.Query<Todo>("SELECT * FROM Todo");
+            connectionString = @"Server=localhost;Database=FcdOne;Trusted_Connection=true;";
         }
-    }
 
-    public void Add(Todo item)
-    {
-        using (IDbConnection dbConnection = GetConnection())
+        public IDbConnection GetConnection()
         {
-            string sQuery = "INSERT INTO Todo (Task, Description)"
-                            + " VALUES(@Task, @Description)";
-            dbConnection.Open();
-            dbConnection.Execute(sQuery, item);
+            return new SqlConnection(connectionString);
         }
-    }
 
-    public void Update(Todo item1)
-    {
-        using (System.Data.IDbConnection dbConnection = GetConnection())
+
+        public IEnumerable<Todo> GetAll()
         {
-            string sQuery = "UPDATE Todo SET Task = @Task,"
-                           + " Description = @Description" 
-                           + " WHERE Todoid = @Todoid";
-            dbConnection.Open();
-            dbConnection.Query(sQuery, item1);
+            using (IDbConnection dbConnection = GetConnection())
+            {
+                dbConnection.Open();
+                return dbConnection.Query<Todo>("SELECT * FROM Todo");
+            }
         }
-    }
 
-    public void Delete(Todo item1)
-    {
-        using (System.Data.IDbConnection dbConnection = GetConnection())
+        public void Add(Todo item)
         {
-            string sQuery = "delete from Todo WHERE Todoid = @Todoid";
-            dbConnection.Open();
-            dbConnection.Query(sQuery, item1);
+            using (IDbConnection dbConnection = GetConnection())
+            {
+                string sQuery = "INSERT INTO Todo (Task, Description, Done)"
+                                + " VALUES(@Task, @Description, @Done)";
+                dbConnection.Open();
+                dbConnection.Execute(sQuery, item);
+            }
         }
-    }
 
+        public Todo View(int item)
+        {
+            using (IDbConnection dbConnection = GetConnection())
+            {
+                string sQuery = "Select * from Todo where Todoid = @id ";
+                dbConnection.Open();
+                List<Todo> todos = dbConnection.Query<Todo>(sQuery, new { id = item }).ToList();
+                
+                return todos.FirstOrDefault();
+            }
+        }
 
+        public void Update(Todo item1)
+        {
+            using (System.Data.IDbConnection dbConnection = GetConnection())
+            {
+                string sQuery = "UPDATE Todo SET Task = @Task,"
+                                + " Done = @Done ,"
+                               + " Description = @Description"
+                               + " WHERE Todoid = @Todoid";
+                dbConnection.Open();
+                dbConnection.Query(sQuery, item1);
+            }
+        }
 
-        
+        public void Delete(Todo item1)
+        {
+            using (System.Data.IDbConnection dbConnection = GetConnection())
+            {
+                string sQuery = "delete from Todo WHERE Todoid = @Todoid";
+                dbConnection.Open();
+                dbConnection.Query(sQuery, item1);
+            }
+        }
     }
 }
