006974397d6259947cc7d77cf20704b81b5806af
Ziyad Umar <ziyad96@gmail.com>
2017-12-26 14:45:00 +0530

    Pull after wrong action on git

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using FantaCode.Todoapi;
+using FantaCode.Todoapi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FantaCode.Todoapi
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+
+        private readonly TodoRepository todoRepository;
+
+        public TodoController()
+        {
+            todoRepository = new TodoRepository();
+        }
+
+
+        //Display all
+        [HttpGet]
+        public IEnumerable<Todo> Get() => todoRepository.GetAll();
+
+
+        [HttpGet("{id}")]
+        public Todo ViewbyID(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                return todoRepository.View(id);
+            }
+            else return null;
+        }
+
+        // POST api/todo
+        //Insert
+        [HttpPost]
+        public void Post([FromBody]Todo todo)
+        {
+            if (ModelState.IsValid)
+                todoRepository.Add(todo);
+        }
+
+        // PUT api/todo/5
+        //Update tasks and descrip
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Todo todo)
+        {
+            todo.TodoId = id;
+            if (ModelState.IsValid)
+                todoRepository.Update(todo);
+        }
+
+        [HttpPut("done/{id}")]
+        public void DoneOnly(int id, [FromBody]Todo todo)
+        {
+            todo.TodoId = id;
+            if (ModelState.IsValid)
+                todoRepository.Done(todo);
+        }
+
+        //Delete
+        [HttpDelete("{id}")]
+        public void Delete(int id, [FromBody]Todo todo)
+        {
+            todo.TodoId = id;
+            if (ModelState.IsValid)
+                todoRepository.Delete(todo);
+        }
+    }
+}
---
+++ Repositories/TodoRepository.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace FantaCode.Todoapi.Repositories
+{
+    public class TodoRepository
+    {
+
+        private string connectionString;
+        public TodoRepository()
+        {
+            connectionString = @"Server=localhost;Database=FcdOne;Trusted_Connection=true;";
+        }
+
+        public IDbConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+
+
+        public IEnumerable<Todo> GetAll()
+        {
+            using (IDbConnection dbConnection = GetConnection())
+            {
+                dbConnection.Open();
+                return dbConnection.Query<Todo>("SELECT * FROM Todo");
+            }
+        }
+
+        public void Add(Todo item)
+        {
+            using (IDbConnection dbConnection = GetConnection())
+            {
+                string sQuery = "INSERT INTO Todo (Task, Description, Done)"
+                                + " VALUES(@Task, @Description , @Done)";
+                dbConnection.Open();
+                dbConnection.Execute(sQuery, item);
+            }
+        }
+
+        public Todo View(int item)
+        {
+            using (IDbConnection dbConnection = GetConnection())
+            {
+                string sQuery = "Select * from Todo where Todoid = @id ";
+                dbConnection.Open();
+                List<Todo> todos = dbConnection.Query<Todo>(sQuery, new { id = item }).ToList();
+                
+                return todos.FirstOrDefault();
+            }
+        }
+
+        public void Update(Todo item1)
+        {
+            using (System.Data.IDbConnection dbConnection = GetConnection())
+            {
+                string sQuery = "UPDATE Todo SET Task = @Task,"
+                                + " Done = @Done ,"
+                               + " Description = @Description"
+                               + " WHERE Todoid = @Todoid";
+                dbConnection.Open();
+                dbConnection.Query(sQuery, item1);
+            }
+        }
+
+        
+        public void Done(Todo item1)
+        {
+            using (System.Data.IDbConnection dbConnection = GetConnection())
+            {
+                string sQuery = "UPDATE Todo SET "
+                                + " Done = @Done "
+                               + " WHERE Todoid = @Todoid";
+                dbConnection.Open();
+                dbConnection.Query(sQuery, item1);
+            }
+        }
+
+        public void Delete(Todo item1)
+        {
+            using (System.Data.IDbConnection dbConnection = GetConnection())
+            {
+                string sQuery = "delete from Todo WHERE Todoid = @Todoid";
+                dbConnection.Open();
+                dbConnection.Query(sQuery, item1);
+            }
+        }
+    }
+}
